8f29bec42c8d6d3c3fca80ab78ef81377149485d
aklales@gmail.com <annaklales@sleekiv-2.local>
2013-06-07 11:24:28 -0400

    Cleaned classical code

--- Propagate_interpolate.m
+++ Propagate_interpolate.m
@@ -1,55 +1,57 @@
 %%  Propagate the particles
-%% COMMENT ALL THE THINGS GITHUB IS COOL BUT DOES IT WORK WHO NEEDS PUNCTUATION LOL
+%   This code classically propagates np particles over our potential
 
-%clear all;
+
+
+
+DontReload = 0;
+
+
+
+if DontReload == 0
+    clear all;
+    
+    load('Potential_4096_1024_i.mat')%loads Force and coords of entries in force matrix
+
+
+    nW = length(xx);
+    nL = length(yy);
+    xV = xx;
+    yV = yy;
+    Fx = Fx_rand + Fx_QPC;
+    Fy = Fy_rand + Fy_QPC;
+    V = Vrand + V_QPC;
+
+    dx = abs(xV(2)-xV(1));  %spatial difference between adjacent points of potential
+    dy = abs(yV(2)-yV(1));
+    Lx = xV(end);           %real dimensions of rectangle 
+    Ly = yV(end);
+
+    buffx = ceil(vel*dt/dx);
+    buffy = ceil(vel*dt/dy);
+end
+
+
 D2 = 1;
 m = 1;
 DoStability = 0;
 PEHO = 8;
 
-np = 10000;
+np = 100;
 
 
 vel = 4;                % initial velocity of particle
 time = 10000;
 dt = 0.01;
 
-%load('Potential_4096_1024_i.mat')%loads Force and coords of entries in force matrix
-
-
-%nW = length(xx);
-%nL = length(yy);
-
-%xV = xx;
-%yV = yy;
-
-%Fx = Fx_rand + Fx_QPC;
-%Fy = Fy_rand + Fy_QPC;
-
-%Vrand = V;
-
-%V = Vrand + V_QPC;
 
 
 
-        % number of grid points along
-
-
-%dx = abs(xV(2)-xV(1));  %spatial difference between adjacent points of potential
-%dy = abs(yV(2)-yV(1));
-%Lx = xV(end);           %real dimensions of rectangle 
-%Ly = yV(end);
-
-
-%[Fx,Fy] = forcify(V,dx,dy);
 if DoStability ==1
-%    [Fxx,Fxy] = forcify(Fx,dx,dy);
-%    [Fyx,Fyy] = forcify(Fy,dx,dy);
     Fxx = Fxx_rand + Fxx_QPC;
     Fyy = Fyy_rand + Fyy_QPC;
     Fxy = Fxy_rand + Fxy_QPC;
 end
-%clear('Fx_rand','Fy_rand','Fxy_rand','Fyy_rand','Fxx_rand','Fx_QPC','Fy_QPC','Fxx_QPC','Fxy_QPC','Fyy_QPC','Vimag','Vrand','V_QPC');
 
 xx = zeros(np,time); 
 yy = zeros(np,time);
@@ -62,7 +64,7 @@ vvyy = zeros(np,time);
     
 
 vy = zeros(np,1); %initial momenta of particles
-vx =  vel*ones(np,1); %zeros(np,1); 
+vx =  vel*ones(np,1); 
 
 %  (center location, standard deviation, number of particles, nothing)
 xxi = normrnd(6,1.5,np,1);
@@ -111,7 +113,7 @@ end
 %% BEGIN PARTICLE-TIME LOOPS
 for ii = 1:np
     if mod(ii,10)==0
-        ii
+        fprintf('On particle %d of %d\n',ii,np)
     end
     for jj = 1:time
         
@@ -120,18 +122,12 @@ for ii = 1:np
         yf = floor(yy(ii,jj)/dy);
         yc = ceil(yy(ii,jj)/dy);
 
-        if ((xf < 25) || (xf > (nW-25)) || (yf < 25) || (yf > (nL-25))) 
+        if ((xf < buffx) || (xf > (nW-buffx)) || (yf < buffy) || (yf > (nL-buffy))) 
                 
             break 
         end                
         xforce = ((xc-(xx(ii,jj)/dx))*(yc-(yy(ii,jj)/dy))*Fx(xf,yf) + (-1*xf + (xx(ii,jj)/dx))*(-1*yf + (yy(ii,jj)/dy))*Fx(xc,yc) + (xc-(xx(ii,jj)/dx))*(-1*yf + (yy(ii,jj)/dy))*Fx(xf,yc) + (-1*xf + (xx(ii,jj)/dx))*(yc-(yy(ii,jj)/dy))*Fx(xc,yf));
         yforce = ((xc-(xx(ii,jj)/dx))*(yc-(yy(ii,jj)/dy))*Fy(xf,yf) + (-1*xf + (xx(ii,jj)/dx))*(-1*yf + (yy(ii,jj)/dy))*Fy(xc,yc) + (xc-(xx(ii,jj)/dx))*(-1*yf + (yy(ii,jj)/dy))*Fy(xf,yc) + (-1*xf + (xx(ii,jj)/dx))*(yc-(yy(ii,jj)/dy))*Fy(xc,yf));
-%         Fx = Force(xint,yint,1);%   which forces should we use?
-%         if D2 == 1;
-%             Fy = Force(xint,yint,2);
-%         else
-%             Fy = 0;
-%         end
         
         vx(ii) = vx(ii) + dt*xforce/m;
         vy(ii) = vy(ii) + dt*yforce/m; 
@@ -150,22 +146,11 @@ for ii = 1:np
             M(ii,jj+1,:,:) = squeeze(M(ii,jj,:,:)) + K*squeeze(M(ii,jj,:,:))*sqrt(vx(ii)^2+vy(ii)^2)*dt;
         end
         
-        
-
         vvxx(ii,jj) = vx(ii);
         vvyy(ii,jj) = vy(ii);
 
     end
 end   
 
-  
-%%
-% if DoStability == 1
-%     save('june5.mat','xx','yy','vvxx','vvyy','M')
-% else
-%     save('june5.mat','xx','yy','vvxx','vvyy')
-% end
-% figure(5)
- %ColorSet = varycolor(np);
- %set(0,'DefaultAxesColorOrder',ColorSet);
- %plot(transpose(xx),transpose(yy),'g.','MarkerSize',1)
+xx = transpose(xx);
+yy = transpose(yy);
